1a486230c15e88e2feb73d957c0badeea5bc5b8c
Yang, Zhuohan <zhuohan.yang@rakuten.com>
2020-09-23 19:14:05 +0800

    add multi-lang support

--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -8,34 +8,7 @@
     </transition> -->
     <div class="normal-height-container gallery-container">
       <div class="container">
-        <div class="china">
-          <div class="chongqing-wrapper">
-            <h1>Chongqing, my hometown.</h1>
-            <p>I was born in Chongqing and raised in Chongqing. There were loads of memories left behind.</p>
-            <div class="chongqing">
-              <div class="chongqing-view-wrapper">
-                <div class="chongqing-view">
-                  <photo-frame class="views-postcard" image-url="chongqing/hongyadong.jpeg"></photo-frame>
-                  <div class="chongqing-view-des-wrapper">
-                    <h3>Scenery</h3>
-                    <p>this is some place holder this is some place holder this is some place holder this is some place holder this is some place holder</p>
-                  </div>
-                </div>
-              </div>
-              <div class="school-wrapper">
-                <div class="school">
-                  <photo-frame class="friends-postcard" image-url="chongqing/IMG_3138-min.jpg"></photo-frame>
-                  <div class="chongqing-friends-des-wrapper">
-                    <h3>Friends</h3>
-                    <p>this is some place holder this is some place holder this is some place holder this is some place holder this is some place holder</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="neimenggu"></div>
-          <div class="xiamen"></div>
-        </div>
+        <china></china>
         <div class="singapore"></div>
         <div class="japan"></div>
         <div class="canada"></div>
@@ -48,7 +21,7 @@
 </template>
 
 <script>
-import PhotoFrame from './parts/PhotoFrame'
+import China from './sessions/China'
 
 export default {
   data () {
@@ -64,10 +37,15 @@ export default {
   methods: {
   },
   components: {
-    PhotoFrame
+    China
   },
   metaInfo: {
     title: 'World In Square - Gallery'
+  },
+  props: {
+    lang: {
+      type: String
+    }
   }
 }
 
@@ -103,67 +81,4 @@ export default {
     width: 500px;
   }
 }
-
-.china {
-  margin-top: 60px;
-}
-
-.chongqing {
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-
-  .chongqing-view-wrapper {
-    width: 60%;
-    padding-right: 5px;
-
-    .chongqing-view {
-      padding: 20px 40px;
-      overflow: hidden;
-      background-color: #fff;
-      border-radius: 30px;
-      height: 400px;
-
-      .views-postcard {
-        position: relative;
-        margin-left: -250px;
-        margin-top: -40px;
-        transform: rotate(12deg);
-        display: block;
-      }
-
-      .chongqing-view-des-wrapper {
-        position: relative;
-        margin-left: 250px;
-        margin-top: -150px;
-      }
-    }
-  }
-
-  .school-wrapper {
-    width: 40%;
-    padding-left: 5px;
-
-    .school {
-      padding: 20px 40px;
-      overflow: hidden;
-      background-color: #fff;
-      border-radius: 30px;
-      height: 400px;
-
-      .friends-postcard {
-        position: relative;
-        margin-left: 54px;
-        margin-top: 190px;
-        transform: rotate(-4deg);
-      }
-
-      .chongqing-friends-des-wrapper {
-        position: relative;
-        margin-top: -470px;
-      }
-    }
-  }
-}
-
 </style>
---
+++ src/components/gallery/sessions/China.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="china">
+    <div class="chongqing-wrapper">
+      <h1>{{ chongqing.title }}</h1>
+      <p>{{ chongqing.description }}</p>
+      <div class="chongqing">
+        <div class="chongqing-view-wrapper">
+          <div class="chongqing-view">
+            <photo-frame class="views-postcard" image-url="chongqing/hongyadong.jpeg"></photo-frame>
+            <div class="chongqing-view-des-wrapper">
+              <h3>{{ chongqing.scenery.title }}</h3>
+              <p>{{ chongqing.scenery.description }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="school-wrapper">
+          <div class="school">
+            <photo-frame class="friends-postcard" image-url="chongqing/IMG_3138-min.jpg"></photo-frame>
+            <div class="chongqing-friends-des-wrapper">
+              <h3>{{ chongqing.friends.title }}</h3>
+              <p>{{ chongqing.friends.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="xiamen-wrapper">
+      <h1>Xiamen, city and sea.</h1>
+      <p>After I graduated from my high school (actually after I finished bridging course), 8 close friends flew to Xianmen, </p>
+    </div>
+    <div class="neimenggu-wrapper">
+      <!-- <h1>Neimenggu, </h1> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import PhotoFrame from '../parts/PhotoFrame'
+import chinaEn from '../../../assets/wording/en/gallery/china.json'
+import chinaCn from '../../../assets/wording/cn/gallery/china.json'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    chongqing () {
+      var chinaText = chinaEn
+      if (navigator.language === 'zh') {
+        chinaText = chinaCn
+      }
+      return {
+        title: chinaText.chongqing.title,
+        description: chinaText.chongqing.description,
+        scenery: {
+          title: chinaText.chongqing.scenery.title,
+          description: chinaText.chongqing.scenery.description
+        },
+        friends: {
+          title: chinaText.chongqing.friends.title,
+          description: chinaText.chongqing.friends.description
+        }
+      }
+    },
+    location () {
+      return navigator.language
+    }
+  },
+  components: {
+    PhotoFrame
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.china {
+  margin-top: 60px;
+}
+
+.chongqing {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+
+  .chongqing-view-wrapper {
+    width: 60%;
+    padding-right: 5px;
+
+    .chongqing-view {
+      padding: 20px 40px;
+      overflow: hidden;
+      background-color: #fff;
+      border-radius: 30px;
+      height: 400px;
+
+      .views-postcard {
+        position: relative;
+        margin-left: -250px;
+        margin-top: -40px;
+        transform: rotate(12deg);
+        display: block;
+      }
+
+      .chongqing-view-des-wrapper {
+        position: relative;
+        margin-left: 250px;
+        margin-top: -150px;
+      }
+    }
+  }
+
+  .school-wrapper {
+    width: 40%;
+    padding-left: 5px;
+
+    .school {
+      padding: 20px 40px;
+      overflow: hidden;
+      background-color: #fff;
+      border-radius: 30px;
+      height: 400px;
+
+      .friends-postcard {
+        position: relative;
+        margin-left: 54px;
+        margin-top: 190px;
+        transform: rotate(-4deg);
+      }
+
+      .chongqing-friends-des-wrapper {
+        position: relative;
+        margin-top: -470px;
+      }
+    }
+  }
+}
+</style>
